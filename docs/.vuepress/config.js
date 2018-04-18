f8a1069358141ffaf6997fe7c60fa56f543526e1
NoManReady <453421041@qq.com>
2018-04-18 17:30:38 +0800

    vuepress
    vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,9 @@ module.exports = {
   description: 'Enjoy Vue',
   host: 'localhost',
   port: '9999',
+  head: [
+    ['link', { rel: 'icon', href: `/favicon.ico` }]
+  ],
   themeConfig: {
     sidebarDepth: 1,
     sidebar: {
